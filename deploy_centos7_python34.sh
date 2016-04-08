a581a556f18d5708187b504252fc0e0b57168bee
root <root@vps266860.ovh.net>
2016-04-08 16:05:15 +0200

    functionnal script

--- deploy_centos7_python34.sh
+++ deploy_centos7_python34.sh
@@ -10,7 +10,9 @@ pip install virtualenv
 
 # install latest mod_wsgi release at https://github.com/GrahamDumpleton/mod_wsgi/releases
 if [ "$(httpd -t -D DUMP_MODULES | grep wsgi_module)" != "" ]; then
+    echo "#############################"
     echo "mod_wsgi déjà installé"
+    read -p "Appuyez sur une touche pour continuer"
 else
     mkdir mod_wsgi_src
     pushd mod_wsgi_src
@@ -22,11 +24,11 @@ else
     echo "LoadModule wsgi_module /usr/lib64/httpd/modules/mod_wsgi.so" > /etc/httpd/conf.modules.d/10-wsgi.conf 
     popd
     rm -fr mod_wsgi_src
-    systemctl start httpd
-    systemctl enable httpd
 
     if [ "$(httpd -t -D DUMP_MODULES | grep wsgi_module)" != "" ]; then
+        echo "#############################"
         echo "mod_wsgi installé avec succès"
+        read -p "Appuyez sur une touche pour continuer"
     fi
 fi
 # mod_wsgi installed
@@ -39,11 +41,8 @@ fi
 # début de la création d'utilisateur
 VALID_USER_RE='^[a-zA-Z][a-zA-Z0-9_\-]{5,}$'
 
-if [ "$DJANGO_USER" == "" ]; then
-    read -p "Nom pour l'utilisateur django : " DJANGO_USER
-fi
-
-echo "1- $DJANGO_USER"
+echo "###############################"
+read -p "Nom pour l'utilisateur django : " DJANGO_USER
 
 while [[ ! "$DJANGO_USER" =~ $VALID_USER_RE ]]
 do
@@ -69,14 +68,15 @@ else
     done
     
     useradd $DJANGO_USER
+    usermod -a -G $DJANGO_USER apache
     echo $DJANGO_USER:$PASSWORD | chpasswd
 fi
 # fin de la création de l'utilisateur
 #####################################
 
-# début de la création du projet django
 #######################################
-DJANGO_USER="django"
+# début de la création du projet django
+echo "#######################"
 read -p "Nom du projet django : " DJANGO_PROJECT
 if [ -d "/home/$DJANGO_USER/$DJANGO_PROJECT" ]; then
     echo "Ce projet existe déja"
@@ -87,9 +87,53 @@ else
     source "$DJANGO_PROJECT"_env/bin/activate
     pip install django 
     django-admin startproject $DJANGO_PROJECT .
-    cd $DJANGO_PROJECT
-    /home/$DJANGO_USER/$DJANGO_PROJECT/manage.py makemigrations
-    /home/$DJANGO_USER/$DJANGO_PROJECT/manage.py migrate
-    /home/$DJANGO_USER/$DJANGO_PROJECT/manage.py createsuperuser
+    sed -i "s/en-us/fr-be/" $DJANGO_PROJECT/settings.py
+    sed -i "s/UTC/Europe\/Brussels/" $DJANGO_PROJECT/settings.py
+    echo "STATIC_ROOT = os.path.join(BASE_DIR,'static/')" >> $DJANGO_PROJECT/settings.py
+    ./manage.py makemigrations
+    ./manage.py migrate
+    ./manage.py createsuperuser
+    ./manage.py collectstatic
     chown -R $DJANGO_USER:$DJANGO_USER /home/$DJANGO_USER/$DJANGO_PROJECT 
+    chmod g+x /home/$DJANGO_USER
 fi
+# fin de la création du projet django
+#####################################
+
+#######################################################
+# début de la configuration du virtual host pour apache
+VALID_HOSTNAME_RE='^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$'
+echo "########################"
+while [[ ! $VIRTUAL_HOST_DOMAIN =~ $VALID_HOSTNAME_RE ]]
+do
+        read -p "Nom de l'hôte virtuel : " VIRTUAL_HOST_DOMAIN
+done
+echo "<VirtualHost *:80>
+    ServerAdmin webmaster@$VIRTUAL_HOST_DOMAIN
+    ServerName $VIRTUAL_HOST_DOMAIN
+    ErrorLog logs/$VIRTUAL_HOST_DOMAIN-error
+    CustomLog logs/$VIRTUAL_HOST_DOMAIN-access common
+
+    Alias /static/ /home/$DJANGO_USER/$DJANGO_PROJECT/static/
+    <Directory /home/$DJANGO_USER/$DJANGO_PROJECT/static>
+        Require all granted
+    </Directory>
+
+    <Directory /home/$DJANGO_USER/$DJANGO_PROJECT/$DJANGO_PROJECT>
+        <Files wsgi.py>
+            Require all granted
+        </Files>
+    </Directory>
+
+    WSGIDaemonProcess $DJANGO_PROJECT user=$DJANGO_USER group=$DJANGO_USER python-path=/home/$DJANGO_USER/$DJANGO_PROJECT:/home/$DJANGO_USER/$DJANGO_PROJECT/"$DJANGO_PROJECT"_env/bin:/home/$DJANGO_USER/$DJANGO_PROJECT/"$DJANGO_PROJECT"_env/lib/python3.4/site-packages/
+    WSGIProcessGroup $DJANGO_PROJECT
+    WSGIScriptAlias / /home/$DJANGO_USER/$DJANGO_PROJECT/$DJANGO_PROJECT/wsgi.py
+
+</VirtualHost>" > /etc/httpd/conf.d/"$VIRTUAL_HOST_DOMAIN".conf
+
+# règle pour selinux
+chcon -R -t httpd_sys_rw_content_t /home/$DJANGO_USER
+systemctl start httpd
+systemctl enable httpd
+# fin de la configuration de l'hôte virtuel
+###########################################
